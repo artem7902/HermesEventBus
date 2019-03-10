9c5a9371c20d758e099169899dd3937644b741d9
Artem Efimenko <aefimenko@agilevision.io>
2019-03-10 14:39:37 +0200

    Migrate to androidx

--- app/src/main/java/xiaofei/library/hermeseventbustest/MainActivity.java
+++ app/src/main/java/xiaofei/library/hermeseventbustest/MainActivity.java
@@ -19,8 +19,8 @@
 package xiaofei.library.hermeseventbustest;
 
 import android.content.Intent;
-import android.os.Process;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -42,7 +42,6 @@ import android.view.View;
  *
  */
 
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
